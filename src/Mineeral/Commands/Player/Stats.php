7bcf9bdfe32d89548283bc4982eeb4dc8455603a
morcheysha77 <laurentmorgan77@gmail.com>
2020-09-30 19:07:05 +0100

    Update line

--- src/Mineeral/Commands/Player/Stats.php
+++ src/Mineeral/Commands/Player/Stats.php
@@ -2,12 +2,14 @@
 
 namespace Mineeral\Commands\Player;
 
-use pocketmine\command\Command;
-use pocketmine\Server;
 use pocketmine\Player;
-use Mineeral\Main;
+
+use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
+
 use pocketmine\network\mcpe\protocol\LevelEventPacket;
+
+use Mineeral\Main;
 
 use onebone\economyapi\EconomyAPI;
 
@@ -22,102 +24,78 @@ class Stats extends Command{
 
     public function execute(CommandSender $sender, string $commandLabel, array $args) : bool
     {
+
         $api = Main::getInstance()->getServer()->getPluginManager()->getPlugin("FormAPI");
-        $perms = Server::getInstance()->getPluginManager()->getPlugin("PurePerms");
+        $perms = Main::getInstance()->getServer()->getPluginManager()->getPlugin("PurePerms");
         $crank = $perms->getUserDataMgr()->getGroup($sender)->getName();
         $money = EconomyAPI::getInstance()->myMoney($sender);
-        $form = $api->createSimpleForm(function (Player $sender, int $data = null){
+
+        $form = $api->createSimpleForm(function (Player $sender, int $data = null) use ($perms, $crank, $money){
             $result = $data;
             if($result === null){
                 return true;
             }
+
             switch($result){
-                case 0;
-                    $perms = Server::getInstance()->getPluginManager()->getPlugin("PurePerms");
-                    $s = $perms->getGroup("Saturne");
-                    $sp = $perms->getGroup("Saturne-Plus");
-                    $e = $perms->getGroup("Eris");
-                    $p = $perms->getGroup("Player");
-                    $crank = $perms->getUserDataMgr()->getGroup($sender)->getName();
-                    $money = EconomyAPI::getInstance()->myMoney($sender);
-                    if($crank == "Player"){
-                        if($money > 10000){
-                            EconomyAPI::getInstance()->reduceMoney($sender, "10000");
-                            $perms->setGroup($sender, $s);
-                            Server::getInstance()->broadcastMessage("§f[§c!§f]§r §fBravo à §4" . $sender->getName() . "§f qui vient d'améloirer son rank à §dSaturne§f !");
-                            $sender->sendTitle("§c-§4 Bravo §c-");
-                            $sender->getLevel()->broadcastLevelEvent($sender->add(0, $sender->getEyeHeight()), LevelEventPacket::EVENT_SOUND_TOTEM);
+                case 0:
+                    switch($crank) {
+                        case "Player":
+                            if($money >= 10000){
+
+                                EconomyAPI::getInstance()->reduceMoney($sender, "10000");
+                                $perms->setGroup($sender, $perms->getGroup("Saturne"));
+                                Main::getInstance()->getServer()->broadcastMessage("§f[§c!§f]§r §fBravo à §4" . $sender->getName() . "§f qui vient d'améloirer son rank à §dSaturne§f !");
+                                $sender->sendTitle("§c-§4 Bravo §c-");
+                                $sender->getLevel()->broadcastLevelEvent($sender->add(0, $sender->getEyeHeight()), LevelEventPacket::EVENT_SOUND_TOTEM);
+                                return true;
+    
+                            } else {
+    
+                                $sender->getLevel()->broadcastLevelEvent($sender->add(0, $sender->getEyeHeight()), LevelEventPacket::EVENT_SOUND_ANVIL_FALL);
+                                $sender->sendMessage("§f[§c!§f]§r §cVous n'avez pas assez d'argent pour ameliorer votre rank!");
+                                return true;
+    
+                            }
+                        break;
+
+                        case "Saturne":
+                            if($money >= 30000){
+                                EconomyAPI::getInstance()->reduceMoney($sender, "30000");
+                                $perms->setGroup($sender, $perms->getGroup("Saturne-Plus"));
+                                Main::getInstance()->getServer()->broadcastMessage("§f[§c!§f]§r §fBravo à §4" . $sender->getName() . "§f qui vient d'améloirer son rank à §5Saturne+§f !");
+                                $sender->sendTitle("§c-§4 Bravo §c-");
+                                $sender->getLevel()->broadcastLevelEvent($sender->add(0, $sender->getEyeHeight()), LevelEventPacket::EVENT_SOUND_TOTEM);
+                                return true;
+                            } else {
+                                $sender->getLevel()->broadcastLevelEvent($sender->add(0, $sender->getEyeHeight()), LevelEventPacket::EVENT_SOUND_ANVIL_FALL);
+                                $sender->sendMessage("§f[§c!§f]§r §cVous n'avez pas assez d'argent pour ameliorer votre rank!");
+                                return true;
+                            }
+                        break;
+
+                        case "Saturne-Plus":
+                            if($money >= 50000){
+                                EconomyAPI::getInstance()->reduceMoney($sender, "50000");
+                                $perms->setGroup($sender, $perms->getGroup("Eris"));
+                                Main::getInstance()->getServer()->broadcastMessage("§f[§c!§f]§r §fBravo à §4" . $sender->getName() . "§f qui vient d'améloirer son rank à §9Eris§f !");
+                                $sender->sendTitle("§c-§4 Bravo §c-");
+                                $sender->getLevel()->broadcastLevelEvent($sender->add(0, $sender->getEyeHeight()), LevelEventPacket::EVENT_SOUND_TOTEM);
+                                return true;
+                            } else {
+                                $sender->getLevel()->broadcastLevelEvent($sender->add(0, $sender->getEyeHeight()), LevelEventPacket::EVENT_SOUND_ANVIL_FALL);
+                                $sender->sendMessage("§f[§c!§f]§r §cVous n'avez pas assez d'argent pour ameliorer votre rank!");
+                                return true;
+                            }
+                        break;
+
+                        default:
+                            $sender->sendMessage("§f§l[§3!§f]§r §fVous avez déjà un rank très haut !");
                             return true;
-                        }
-                        if($money == 10000){
-                            EconomyAPI::getInstance()->reduceMoney($sender, "10000");
-                            $perms->setGroup($sender, $s);
-                            Server::getInstance()->broadcastMessage("§f[§c!§f]§r §fBravo à §4" . $sender->getName() . "§f qui vient d'améloirer son rank à §dSaturne§f !");
-                            $sender->sendTitle("§c-§4 Bravo §c-");
-                            $sender->getLevel()->broadcastLevelEvent($sender->add(0, $sender->getEyeHeight()), LevelEventPacket::EVENT_SOUND_TOTEM);
-                            return true;
-                        }
-                        if($money < 10000){
-                            $sender->getLevel()->broadcastLevelEvent($sender->add(0, $sender->getEyeHeight()), LevelEventPacket::EVENT_SOUND_ANVIL_FALL);
-                            $sender->sendMessage("§f[§c!§f]§r §cVous n'avez pas assez d'argent pour ameliorer votre rank!");
-                            return true;
-                        }
-                    }
-                    if($crank == "Saturne"){
-                        if($money > 30000){
-                            EconomyAPI::getInstance()->reduceMoney($sender, "30000");
-                            $perms->setGroup($sender, $sp);
-                            Server::getInstance()->broadcastMessage("§f[§c!§f]§r §fBravo à §4" . $sender->getName() . "§f qui vient d'améloirer son rank à 
-§5Saturne+§f !");
-                            $sender->sendTitle("§c-§4 Bravo §c-");
-                            $sender->getLevel()->broadcastLevelEvent($sender->add(0, $sender->getEyeHeight()), LevelEventPacket::EVENT_SOUND_TOTEM);
-                            return true;
-                        }
-                        if($money == 30000){
-                            EconomyAPI::getInstance()->reduceMoney($sender, "30000");
-                            $perms->setGroup($sender, $sp);
-                            Server::getInstance()->broadcastMessage("§f[§c!§f]§r §fBravo à §4" . $sender->getName() . "§f qui vient d'améloirer son rank à §5Saturne+§f !");
-                            $sender->sendTitle("§c-§4 Bravo §c-");
-                            $sender->getLevel()->broadcastLevelEvent($sender->add(0, $sender->getEyeHeight()), LevelEventPacket::EVENT_SOUND_TOTEM);
-                            return true;
-                        }
-                        if($money < 20000){
-                            $sender->getLevel()->broadcastLevelEvent($sender->add(0, $sender->getEyeHeight()), LevelEventPacket::EVENT_SOUND_ANVIL_FALL);
-                            $sender->sendMessage("§f[§c!§f]§r §cVous n'avez pas assez d'argent pour ameliorer votre rank!");
-                            return true;
-                        }
-                    }
-                    if($crank == "Saturne-Plus"){
-                        if($money > 50000){
-                            EconomyAPI::getInstance()->reduceMoney($sender, "50000");
-                            $perms->setGroup($sender, $e);
-                            Server::getInstance()->broadcastMessage("§f[§c!§f]§r §fBravo à §4" . $sender->getName() . "§f qui vient d'améloirer son rank à §9Eris§f !");
-                            $sender->sendTitle("§c-§4 Bravo §c-");
-                            $sender->getLevel()->broadcastLevelEvent($sender->add(0, $sender->getEyeHeight()), LevelEventPacket::EVENT_SOUND_TOTEM);
-                            return true;
-                        }
-                        if($money == 50000){
-                            EconomyAPI::getInstance()->reduceMoney($sender, "50000");
-                            $perms->setGroup($sender, $e);
-                            Server::getInstance()->broadcastMessage("§f[§c!§f]§r §fBravo à §4" . $sender->getName() . "§f qui vient d'améloirer son rank à §9Eris§f !");
-                            $sender->sendTitle("§c-§4 Bravo §c-");
-                            $sender->getLevel()->broadcastLevelEvent($sender->add(0, $sender->getEyeHeight()), LevelEventPacket::EVENT_SOUND_TOTEM);
-                            return true;
-                        }
-                        if($money < 50000){
-                            $sender->getLevel()->broadcastLevelEvent($sender->add(0, $sender->getEyeHeight()), LevelEventPacket::EVENT_SOUND_ANVIL_FALL);
-                            $sender->sendMessage("§f[§c!§f]§r §cVous n'avez pas assez d'argent pour ameliorer votre rank!");
-                            return true;
-                        }
-                    }
-                    if($crank == "Eris"){
-                        $sender->sendMessage("§f§l[§3!§f]§r §fVous avez déjà le dernier rank !");
-                        return true;
+                        break;
                     }
                 break;
 
                 case 1;
-
                 break;
             }
         });
